0b8e0d8835093cbe407c9ca88c4acc9d7896807d
anagtv <ana.garcia-tabares.valdivieso@cern.ch>
2020-12-18 17:26:09 +0100

    Simplyfing writing_files function, includin a loop

--- python_analysis_dataframe_20200416.py
+++ python_analysis_dataframe_20200416.py
@@ -107,14 +107,22 @@ def writing_files(input_path):
     gimbal_control_app_path_output = os.path.join(input_path,"all_scans_gimbal")
     pendant_ui_app_path_output = os.path.join(input_path,"all_scans_pendant")
     system_manager_app_path_output =  os.path.join(input_path,"all_scans_system")
-    with open(rotor_control_app_path, "r") as reader, open(rotor_control_app_path_output, "w") as writer: 
-          writer.writelines(line for line in reader if _check_scan(line,"#f11"))
-    with open(gimbal_control_app_path, "r") as reader, open(gimbal_control_app_path_output, "w") as writer: 
-          writer.writelines(line for line in reader if _check_scan(line,"#z11"))
-    with open(pendant_ui_app_path, "r") as reader, open(pendant_ui_app_path_output, "w") as writer: 
-          writer.writelines(line for line in reader if _check_scan(line,"#i11"))
-    with open(system_manager_app_path, "r") as reader, open(system_manager_app_path_output, "w") as writer: 
-          writer.writelines(line for line in reader if _check_scan(line,"#s11"))
+    files_control_input = [rotor_control_app_path,gimbal_control_app_path,pendant_ui_app_path,system_manager_app_path]
+    files_control_output = [rotor_control_app_path_output,gimbal_control_app_path_output,pendant_ui_app_path_output,system_manager_app_path_output]
+    codes_control = ["#f11","#z11","#i11","#s11"]
+    for i in range(len(files_control_input)):
+        print (files_control_input[i])
+        print (files_control_output[i])
+        with open(files_control_input[i], "r") as reader, open(files_control_output[i], "w") as writer: 
+          writer.writelines(line for line in reader if _check_scan(line,codes_control[i]))
+    #with open(rotor_control_app_path, "r") as reader, open(rotor_control_app_path_output, "w") as writer: 
+    #      writer.writelines(line for line in reader if _check_scan(line,"#f11"))
+    #with open(gimbal_control_app_path, "r") as reader, open(gimbal_control_app_path_output, "w") as writer: 
+    #      writer.writelines(line for line in reader if _check_scan(line,"#z11"))
+    #with open(pendant_ui_app_path, "r") as reader, open(pendant_ui_app_path_output, "w") as writer: 
+    #      writer.writelines(line for line in reader if _check_scan(line,"#i11"))
+    #with open(system_manager_app_path, "r") as reader, open(system_manager_app_path_output, "w") as writer: 
+    #      writer.writelines(line for line in reader if _check_scan(line,"#s11"))
     return rotor_control_app_path_output,gimbal_control_app_path_output,pendant_ui_app_path_output,system_manager_app_path_output
 
 
