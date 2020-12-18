eced5c231d46174d28ccf3f2620e3ff54d5f528a
anagtv <ana.garcia-tabares.valdivieso@cern.ch>
2020-12-18 17:27:53 +0100

    Removing extract functions

--- my_fav_gui_airo.py
+++ my_fav_gui_airo.py
@@ -1,16 +1,3 @@
-#! /usr/bin/env python3
-#  -*- coding:utf-8 -*-
-###############################################################
-# kenwaldek                           MIT-license
-
-# Title: PyQt5 lesson 14              Version: 1.0
-# Date: 09-01-17                      Language: python3
-# Description: pyqt5 gui and opening files
-# pythonprogramming.net from PyQt4 to PyQt5
-###############################################################
-# do something
-
-
 import sys
 from PyQt5.QtCore import QCoreApplication, Qt
 from PyQt5.QtGui import QIcon, QColor,QStandardItemModel
@@ -51,7 +38,6 @@ ROTOR_SCAN = ["ReconstructionManager received scan protocol: ScanProtocol:","Cat
 ROTOR_BEST = ["AirQualifyClass is unable to Load Candidate Scan","Too many bad detectors found!","Pleora Device Open Failed","NoGPUAvailable","ReconstructionInitializer.FullSetup failed","New Mode From X-Ray Control Board:",
 "ReconstructionManager.TransitionStates:","Stopping Rotor Control Application","Starting Rotor Control Application","ConfigStore.Set: History/MaintenanceScans ->"]
 ROTOR_MOTION = ["MotorController.LogRotorData"]
-
 SYSTEM_SCAN = ["MQM message received: @D#s11 Prepared ","MQM send message: #*11 Prepared ","CsScriptEngine.RunScript, script=Prepare, args= ","-- Start PrepareFor","-- PrepareFor ","SystemManager.OnScriptCompleted SUCCESS","MQM message received: @D#s11 Completed ",
 "MQM send message: #*11 Completed ","SystemStatusAgglomerator raising ReadyToScan event ","CsScriptEngine.RunScript, script=StartScan, args= ","SystemManager.OnScriptCompleted SUCCESS, id=G ","Component Status Changed: Address x | Completed | Transitioning | ","Component Status Changed: Address r | Completed | OK | ",
 "Component Status Changed: Address x | Completed | Transitioning | ","Component Status Changed: Address x | Completed | Transitioning | ","Component Status Changed: Address x | Completed | OK | WarmupScan | ","CsScriptEngine.RunScript, script=CompleteScan",
@@ -60,15 +46,11 @@ SYSTEM_BEST = ["MaintenanceManager recording WarmUp at","MaintenanceManager reco
 "MaintenanceManager recording WarmUp at","MaintenanceManager recording GainCal","MaintenanceManager recording EstopTest","MaintenanceManager recording QCSAB","MaintenanceManager recording QCSTB","System address n not registered with queue manager","System address x not registered with queue manager",
 "System address g not registered with queue manager","System address r not registered with queue manager","System address f not registered with queue manager","PrepareForScan Script Failed","Error loading Config file",
 "MIGimbal n","MIGimbal p","MIGimbal z","MIGimbal t", "MIRotor n", "MIRotor x", "MIRotor g", "MIRotor r","Script Failed"]
-
 PENDANT_SCAN = ["UI: StateMachine: Fire"]
 PENDANT_BEST =  ["Failed DB Integrity Check: DB Corruption Found","UI: StateMachine: Fire"]
 GIMBAL_SCAN = ["Handling Client Message: #t11 Prepared","New Scan From Power Control Board: AA","New Scan From Power Control Board: DD","Handling Client Message: p11 Completed","New Scan From Power Control Board: XKX","New Scan From Power Control Board: XKS",
 "New Scan From Power Control Board: XM","New Scan From Power Control Board: DD","New Scan From Power Control Board: ZZ"]
 GIMBAL_BEST = []
-
-
-
 SHUTDOWN_CODES =["FsI","FsN","FsP","FsB","FsW","FsF","FsK","FsZ"]
 SHUTDOWN_CODES_MEANING = ["1st turn turning on after board is flashed",
 "power to the control board failed unexpectedly",
@@ -251,18 +233,6 @@ class window(QMainWindow):
             self.textEdit_files_2.setText(text)
 
 
-    def extract_best(self):
-        file = open(str(self.file_to_display), "r")
-        with file:
-            text = file.read()
-            self.textEdit_files.setText(text)
-
-
-    def extract_scan(self):
-        file = open(str(self.file_to_display), "r")
-        with file:
-            text = file.read()
-            self.textEdit_files.setText(text)
 
     def filter_general_file(self,path_scan,path_best,file_input,notebook,notebook_2,filter_scan,filter_best):
         python_analysis_dataframe_20200416.summarising_file(self,file_input,path_scan,filter_scan)
